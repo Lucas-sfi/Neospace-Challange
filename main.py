efdb291f2cc7df5abdc0dda75d2e61a6364dbcc7
lucas Figueiredo <lucasilvaf09@gmail.com>
2025-05-06 16:37:00 -0300

    first commit of the challange

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from transformers import pipeline
+
+generator = pipeline(task="text-generation", model='deepseek-ai/DeepSeek-Prover-V2-671B')
+generator("Hello, I'm a language model", max_length = 30, num_return_sequences=3)
+
+
 
